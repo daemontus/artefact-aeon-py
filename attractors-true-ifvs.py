78fda6268f495993f2167babe0241d8049c29b07
Samuel Pastva <sam.pastva@gmail.com>
2022-02-23 21:09:04 +0100

    Add iFVS benchmark script.

---
+++ attractors-true-ifvs.py
@@ -0,0 +1,23 @@
+import sys
+import os
+
+from biodivine_aeon import *
+from pathlib import Path
+
+model_path = sys.argv[1]
+print("Model path", model_path)
+
+model_string = Path(model_path).read_text()
+model = BooleanNetwork.from_aeon(model_string)
+model = fix_inputs_true(model)
+model = set_canonical_names(model)
+
+print("Loaded model with", model.num_vars(), "variables")
+
+name = os.path.basename(model_path)
+Path("./iFVS-ABN-v0.1/networks/"+name.replace("aeon", "bnet")).write_text(model.to_bnet())
+
+os.chdir('iFVS-ABN-v0.1')
+code = os.system('java -jar iFVS-ABN.jar '+name.replace("aeon", "bnet"))
+print("Exit code", code)
+exit(code)
