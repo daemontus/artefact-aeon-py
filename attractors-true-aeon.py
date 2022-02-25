fca64e73b17145180bfba6d963cd512718b3cc72
Onrej Huvar <xhuvar@psyche07.fi.muni.cz>
2022-02-25 20:03:55 +0100

    Modify model handling in tool scripts

--- attractors-true-aeon.py
+++ attractors-true-aeon.py
@@ -4,13 +4,14 @@ from biodivine_aeon import *
 from pathlib import Path
 from utils import fix_inputs_true, set_canonical_names
 
+
 model_path = sys.argv[1]
 print("Model path", model_path)
 
 model_string = Path(model_path).read_text()
 model = BooleanNetwork.from_aeon(model_string)
-model = fix_inputs_true(model)
-model = set_canonical_names(model)
+#model = fix_inputs_true(model)
+#model = set_canonical_names(model)
 
 print("Loaded model with", model.num_vars(), "variables.")
 
@@ -20,6 +21,6 @@ attractors = find_attractors(graph)
 
 total = 0.0
 for attr in attractors:
-	total = total + attr.cardinality()	
+	total = total + attr.cardinality()
 
 print("Total attractor state-colour pairs:", total)
--- attractors-true-pystable.py
+++ attractors-true-pystable.py
@@ -9,15 +9,16 @@ from utils import fix_inputs_true, set_canonical_names
 model_path = sys.argv[1]
 print("Model path", model_path)
 
-model_string = Path(model_path).read_text()
-model = BooleanNetwork.from_aeon(model_string)
-model = fix_inputs_true(model)
-model = set_canonical_names(model)
+#model_string = Path(model_path).read_text()
+#model = BooleanNetwork.from_aeon(model_string)
+#model = fix_inputs_true(model)
+#model = set_canonical_names(model)
 
-print("Loaded model with", model.num_vars(), "variables.")
+#print("Loaded model with", model.num_vars(), "variables.")
 
 
-primes = sm.format.create_primes(model.to_bnet())
+#primes = sm.format.create_primes(model.to_bnet())
+primes = sm.format.create_primes(Path(model_path.replace("aeon", "bnet")).read_text())
 
 ar = sm.AttractorRepertoire.from_primes(primes)
 ar.summary()
